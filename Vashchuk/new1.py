b733f9076497e6ab382cdda7c54e927b21b3aa4f
VashchukV <145018437+VashchukV@users.noreply.github.com>
2023-12-18 21:50:47 +0200

    update

--- Vashchuk/new1.py
+++ Vashchuk/new1.py
@@ -83,8 +83,8 @@ class Operation:
 
 bank = Bank()
 
-client1 = Client("John Doe", "123456789", "1234", initial_balance=500)
-client2 = Client("Jane Smith", "987654321", "5678", initial_balance=1000)
+client1 = Client("John Doe", "123456789", "1454", initial_balance=500)
+client2 = Client("Jane Smith", "987654321", "5578", initial_balance=1000)
 
 atm1 = ATM(5000)
 atm2 = ATM(3000)
@@ -97,7 +97,7 @@ bank.add_atm(atm2)
 
 print("Client 1 balance:", bank.atms[0].check_balance(client1))
 
-if bank.verify_pin(client1, "1234") and bank.atms[0].withdraw_cash(client1, 200):
+if bank.verify_pin(client1, "1321") and bank.atms[0].withdraw_cash(client1, 200):
     print("Withdrawal successful. Remaining balance:", client1.get_balance())
     print("ATM 1 cash available:", bank.atms[0].cash_available)
 else:
