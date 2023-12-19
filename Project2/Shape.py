56ae98702f255eb72ef8d7a263883e1ff13cc53e
VashchukV <145018437+VashchukV@users.noreply.github.com>
2023-12-19 21:33:40 +0200

    Shapes

---
+++ Project2/Shape.py
@@ -0,0 +1,9 @@
+class Shape(ABC):
+    def init(self):
+        pass
+    def calculate_area(self):
+        pass
+    def calculate_perimeter(self):
+        pass
+    def display_info(self):
+        pass
