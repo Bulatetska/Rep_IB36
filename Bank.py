3ace1def4649615340396b50e3758b63ab333eb0
VashchukV <145018437+VashchukV@users.noreply.github.com>
2023-12-19 19:18:31 +0200

    Class Bank

---
+++ Bank.py
@@ -0,0 +1,30 @@
+class Bank:
+    def init(self):
+        self.clients = []
+        self.atms = []
+        self.operations = []
+
+    def add_client(self, client):
+        self.clients.append(client)
+
+    def add_atm(self, atm):
+        self.atms.append(atm)
+
+    def find_client_by_account_number(self, account_number):
+        for client in self.clients:
+            if client.account_number == account_number:
+                return client
+        return None
+
+    def verify_pin(self, client, pin):
+        return client.pin == pin
+
+    def transfer_money(self, sender, receiver_account_number, amount):
+        receiver = self.find_client_by_account_number(receiver_account_number)
+        if receiver and sender.balance >= amount:
+            sender.withdraw(amount)
+            receiver.deposit(amount)
+            self.operations.append(Operation(sender, "Transfer", amount))
+            self.operations.append(Operation(receiver, "Deposit", amount))
+            return True
+        return False
