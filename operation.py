78d4d0b902acf7213014211396a2cb48da13c58b
Bogdan <97546031+bogdan-sage@users.noreply.github.com>
2023-12-19 18:53:57 +0200

    operation

---
+++ operation.py
@@ -0,0 +1,6 @@
+class Operation:
+    def __init__(self, client, operation_type, amount):
+        self.client = client
+        self.operation_type = operation_type
+        self.amount = amount
+        self.date_time = datetime.now()
